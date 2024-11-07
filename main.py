0aa19358bafd6c9427983a6eb71e6f2adc758aa1
Samuel <61188554+samibanks01@users.noreply.github.com>
2024-11-06 19:22:20 -0900

    Create main.py
    Added files

---
+++ main.py
@@ -0,0 +1,92 @@
+print("Animal Sounds!")
+
+print()
+
+exit = ""
+
+while exit != "yes":
+
+  animal = input("What animal do you want? ")
+
+  if animal == "cow":
+
+    print("A cow goes moo.")
+
+  elif animal == "dog":
+
+    print("A dog goes woof.")
+
+  elif animal == "cat":
+
+    print("A cat goes meow.")
+
+  elif animal == "sheep":
+
+    print("A sheep goes baa.")
+
+  elif animal == "horse":
+
+    print("A horse goes neigh.")
+
+  elif animal == "pig":
+
+    print("A pig goes oink.")
+
+  elif animal == "chicken":
+
+    print("A chicken goes cluck.")
+
+  elif animal == "duck":
+
+    print("A duck goes quack.")
+
+  elif animal == "goat":
+
+    print("A goat goes baa.")
+
+  elif animal == "lion":
+
+    print("A lion goes roar.")
+
+  elif animal == "tiger":
+
+    print("A tiger goes roar.")
+
+  elif animal == "bear":
+
+    print("A bear goes growl.")
+
+  elif animal == "wolf":
+
+    print("A wolf goes howl.")
+
+  elif animal == "frog":
+
+    print("A frog goes ribbit.")
+
+  elif animal == "elephant":
+
+    print("An elephant goes trumpet.")
+
+  elif animal == "giraffe":
+
+    print("A giraffe goes hum.")
+
+  elif animal == "monkey":
+
+    print("A monkey goes ooh ooh ah ah.")
+
+  elif animal == "hawk":
+
+    print("A hawk goes caw.")  
+
+  elif animal == "owl":
+
+    print("An owl goes hoot.")
+
+  exit = input("Do you want to exit? ")
+
+else:
+  print("I don't know that animal.")
+  print("Goodbye!")
+    
